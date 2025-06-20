4c05755f0b52a4db86e9182afb6d1d0ed5b7159a
N9C9 <nicolo.casertano@gmail.com>
2025-06-20 17:37:07 +0200

    aggiorna con l'entity admin

--- demo/src/main/java/com/example/app/models/entities/Admin.java
+++ demo/src/main/java/com/example/app/models/entities/Admin.java
@@ -1,4 +1,68 @@
 package com.example.app.models.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
 public class Admin {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer admId;
+
+    @Column(name = "admin_name")
+    private String admName;
+
+    @Column(name = "admin_password")
+    private String admPassword;
+
+
+    @OneToMany(mappedBy = "admin")
+    private Integer workId;
+
+    public Admin(Integer admId, String admName, String admPassword, Integer workId) {
+        this.admId = admId;
+        this.admName = admName;
+        this.admPassword = admPassword;
+        this.workId = workId;
+    }
+
+    public Admin() {
+    }
+
+    //GETTER E SETTER
+
+
+    public Integer getAdmId() {
+        return admId;
+    }
+
+    public void setAdmId(Integer admId) {
+        this.admId = admId;
+    }
+
+    public String getAdmName() {
+        return admName;
+    }
+
+    public void setAdmName(String admName) {
+        this.admName = admName;
+    }
+
+    public String getAdmPassword() {
+        return admPassword;
+    }
+
+    public void setAdmPassword(String admPassword) {
+        this.admPassword = admPassword;
+    }
+
+    public Integer getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(Integer workId) {
+        this.workId = workId;
+    }
 }
